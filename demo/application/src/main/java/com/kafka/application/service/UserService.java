7f9a098d0e622ceb561e3928ede13f1cca708760
Nikolay Shelevoy <runtycybin@gmail.com>
2025-10-01 16:05:56 +0200

    user service and its implementation made to use a dto

--- demo/application/src/main/java/com/kafka/application/service/UserService.java
+++ demo/application/src/main/java/com/kafka/application/service/UserService.java
@@ -3,14 +3,15 @@ package com.kafka.application.service;
 import java.util.List;
 
 import com.kafka.domain.User;
+import com.kafka.domain.UserUpdateRequestDto;
 
 public interface UserService {
 
-  void createUser(String name, String email);
+  void createUser(UserUpdateRequestDto userRequest);
 
   void deleteUser(String userId);
 
-  boolean updateUser(String name, String email);
+  boolean updateUser(UserUpdateRequestDto userRequest);
 
   User getUserById(String userId);
 
