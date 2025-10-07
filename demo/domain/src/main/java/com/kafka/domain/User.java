d4bd7f60844c289b57f075dcd8ab1f96847dc5d0
Nikolay Shelevoy <runtycybin@gmail.com>
2025-10-07 10:49:50 +0200

    user service update

--- demo/domain/src/main/java/com/kafka/domain/User.java
+++ demo/domain/src/main/java/com/kafka/domain/User.java
@@ -20,6 +20,14 @@ public class User {
     return email;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   @Override
   public String toString() {
     return "User{" +
