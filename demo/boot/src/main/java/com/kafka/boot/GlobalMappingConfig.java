d17cb2847fbf9124e84074e073aa58e23ef927b5
Nikolay Shelevoy <runtycybin@gmail.com>
2025-10-15 19:55:20 +0200

    [map_struct_introduced]: global mapping configuration

---
+++ demo/boot/src/main/java/com/kafka/boot/GlobalMappingConfig.java
@@ -0,0 +1,15 @@
+package com.kafka.boot;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public class GlobalMappingConfig {
+}
